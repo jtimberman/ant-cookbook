c137c0a085caada9d4575682c327d940d6a51bc4
Jennifer Davis <iennae@gmail.com>
2016-10-31 20:12:54 -0700

    Fix for issue #9 to remove the extra forward slash Signed-off-by: Jennifer Davis <iennae@gmail.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,7 +18,7 @@
 
 default['ant']['version']        = '1.9.7'
 default['ant']['home']           = '/usr/local/ant'
-default['ant']['url']            = 'http://archive.apache.org/dist/ant/binaries/'
+default['ant']['url']            = 'http://archive.apache.org/dist/ant/binaries'
 default['ant']['install_method'] = 'package'
 
 default['ant']['libraries']      = { 'ant-contrib' => 'http://search.maven.org/remotecontent?filepath=ant-contrib/ant-contrib/1.0b3/ant-contrib-1.0b3.jar' }
